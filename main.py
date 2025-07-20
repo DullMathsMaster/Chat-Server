a18ceea21c4231dd68e43a517b5d1921bcc0f519
xhu6 <143360206+xhu6@users.noreply.github.com>
2025-07-20 22:08:36 +0000

    Support private chats

--- main.py
+++ main.py
@@ -1,83 +1,52 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 # https://fastapi.tiangolo.com/advanced/websockets/#handling-disconnections-and-multiple-clients
 
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Chat</title>
-    </head>
-    <body>
-        <h1>WebSocket Chat</h1>
-        <h2>Your ID: <span id="ws-id"></span></h2>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-        <ul id='messages'>
-        </ul>
-        <script>
-            var client_id = Date.now()
-            document.querySelector("#ws-id").textContent = client_id;
-            var ws = new WebSocket(`ws://localhost:8000/ws/${client_id}`);
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
-            function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }
-        </script>
-    </body>
-</html>
-"""
-
-
-class ConnectionManager:
+class Manager:
     def __init__(self):
-        self.active_connections: list[WebSocket] = []
+        self.users: dict[int, list[WebSocket]] = {}
 
-    async def connect(self, websocket: WebSocket):
+    async def connect(self, user_id: int, websocket: WebSocket):
         await websocket.accept()
-        self.active_connections.append(websocket)
+        
+        if user_id not in self.users:
+            self.users[user_id] = []
 
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
+        self.users[user_id].append(websocket) 
 
-    async def send_personal_message(self, message: str, websocket: WebSocket):
-        await websocket.send_text(message)
+    def disconnect(self, user_id: int, websocket: WebSocket):
+        if user_id not in self.users:
+            return
 
-    async def broadcast(self, message: str):
-        for connection in self.active_connections:
-            await connection.send_text(message)
+        # O(N) to remove but unlikely to be large
+        # May also leave empty list which uses up memory        
+        self.users[user_id].remove(websocket)
 
+    async def send(self, user_id: int, data: dict):
+        if user_id not in self.users:
+            return
+        
+        for connection in self.users[user_id]:
+            await connection.send_json(data)
 
-manager = ConnectionManager()
+manager = Manager()
 
+@app.websocket("/ws/{user_id}")
+async def websocket_endpoint(websocket: WebSocket, user_id: int):
+    await manager.connect(user_id, websocket)
 
-@app.get("/")
-async def get():
-    return HTMLResponse(html)
-
-
-@app.websocket("/ws/{client_id}")
-async def websocket_endpoint(websocket: WebSocket, client_id: int):
-    await manager.connect(websocket)
-    await manager.broadcast(f"Client #{client_id} joined the chat")
     try:
+        await manager.send(user_id, f"#{user_id} joined the chat")
+
         while True:
-            data = await websocket.receive_text()
-            await manager.broadcast(f"Client #{client_id} says: {data}")
+            # Can error if JSON invalid.
+            data = await websocket.receive_json()
+            print(user_id, data)
+
+            for user in data["users"]:
+                await manager.send(user, data)
+        
     except WebSocketDisconnect:
-        manager.disconnect(websocket)
-        await manager.broadcast(f"Client #{client_id} left the chat")
+        manager.disconnect(user_id, websocket)
