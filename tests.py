5dba81c0761f4043b2b1d15fdbb0271c0ed40bfa
xhu6 <143360206+xhu6@users.noreply.github.com>
2025-07-31 18:27:27 +0000

    Test send[direct]

--- tests.py
+++ tests.py
@@ -1,12 +1,44 @@
-from unittest import IsolatedAsyncioTestCase
+from unittest import IsolatedAsyncioTestCase, main
+from json import loads
 
-from chat_server.manager import Manager
 from chat_server.db import DB
 from chat_server.handler import RequestHandler
 
-manager = Manager()
+
+class DummyManager:
+    def __init__(self):
+        self.log = []
+
+    async def send(self, user_id: int, data: str):
+        self.log.append((user_id, loads(data)))
+
+
 db = DB("sqlite+pysqlite:///data/tests.db")
-handler = RequestHandler(manager, db)
 
 
-class Test(IsolatedAsyncioTestCase): ...
+class HandlerTest(IsolatedAsyncioTestCase):
+    def setUp(self):
+        self.manager = DummyManager()
+        self.handler = RequestHandler(self.manager, db)
+
+    async def test_send_direct(self):
+        await self.handler.send_direct(
+            1, {"type": "send[direct]", "recipient": 2, "content": "hello"}
+        )
+
+        users = {1, 2}
+
+        for recipient, data in self.manager.log:
+            # Ensure content is as expected
+            self.assertEqual(data["sender"], 1)
+            self.assertEqual(data["recipient"], 2)
+            self.assertEqual(data["content"], "hello")
+
+            users.remove(recipient)
+
+        # Ensure every user in DM receives message
+        self.assertEqual(len(users), 0)
+
+
+if __name__ == "__main__":
+    main()
